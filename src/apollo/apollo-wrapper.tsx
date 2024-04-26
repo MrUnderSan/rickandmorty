5052406d3ca2a69b41e91f87efaf65713a7954ec
MrUnderSan <MrUnderSan@gmail.com>
2024-04-26 15:54:12 +0300

    feat: add apollo client, apollo wrapper

---
+++ src/apollo/apollo-wrapper.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { ApolloLink, HttpLink } from '@apollo/client';
+import {
+  ApolloNextAppProvider,
+  NextSSRApolloClient,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from '@apollo/experimental-nextjs-app-support/ssr';
+import { PropsWithChildren } from 'react';
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GQL_ENDPOINT,
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === 'undefined'
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
---
+++ src/apollo/index.ts
@@ -0,0 +1,2 @@
+export { getClient } from './apollo-client';
+export { ApolloWrapper } from './apollo-wrapper';
